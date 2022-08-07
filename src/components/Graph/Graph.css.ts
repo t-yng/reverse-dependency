eb361dca4be8e44ff8f5be8ba18319c501bc79d5
t-yng <t-yng@users.noreply.github.com>
2022-08-07 21:08:46 +0900

    依存グラフが巨大な時を考慮して表示を調整 (#2)

--- src/components/Graph/Graph.css.ts
+++ src/components/Graph/Graph.css.ts
@@ -1,8 +1,9 @@
 import { style } from "@vanilla-extract/css";
 
 export const graph = style({
-  display: "flex",
-  justifyContent: "center",
-  alignItems: "center",
   flex: 1,
+  width: "100%",
+  height: "100%",
+  minHeight: "100vh",
+  cursor: "move",
 });
